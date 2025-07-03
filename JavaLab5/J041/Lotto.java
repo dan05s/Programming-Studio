400a5a9f0c6f3be971d3911130fcc791d760f620
Dan05s <dan05s@naver.com>
2025-07-03 22:25:05 +0900

    Edit J041

---
+++ JavaLab5/J041/Lotto.java
@@ -0,0 +1,48 @@
+package JavaLab5.J041;
+
+import java.util.Random;
+
+public class Lotto {
+
+    Random rand = new Random();
+
+    private int[] numbers;
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+
+    public Lotto(){
+        generateUniqueNumbers();
+    }
+
+    public void printNumbers() {
+        for (int i = 0; i < numbers.length; i++) {
+            if (i > 0) System.out.print(" ");
+            System.out.print(numbers[i]);
+        }
+        System.out.println();
+    }
+
+    public void remakeAuto() {
+        generateUniqueNumbers();
+    }
+
+    private void generateUniqueNumbers() {
+        boolean[] used = new boolean[46]; // 1~45
+        int count = 0;
+
+        while (count < 6) {
+            int num = rand.nextInt(45) + 1; // 1~45
+            if (!used[num]) {
+                numbers[count++] = num;
+                used[num] = true;
+            }
+        }
+    }
+}
--- JavaLab5/J041/Main.java
+++ JavaLab5/J041/Main.java
@@ -1,3 +1,17 @@
+package JavaLab5.J041;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    void run(){
+        Lotto lt = new Lotto();
+
+        lt.printNumbers();
+
+        lt.remakeAuto();
+
+        lt.printNumbers();
+    }
 }
